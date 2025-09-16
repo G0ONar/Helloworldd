c875183ba3ad95c3d9b916a8913e240482a24ada
G0ONar <142931976+G0ONar@users.noreply.github.com>
2025-09-16 13:20:07 -0700

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+public class HelloWorld
+ {
+    public syatic void main(string[] args)[]{
+        int age =16;
+        String name="Gunnar Pettit";
+        System.out.println("Hellow world, I am "+name);
+        System.out.println("I am "+age+"years old.");
+        System.out.println("I have 3 siblings");
+        System.out.println("Ryker")
+        System.out.println("Maverick")
+        System.out.println("and Nora")
+        System.out.println("My parents are")
+        System.out.println("Beth")
+        System.out.println("and Kyle")
+    }
+
+}
+

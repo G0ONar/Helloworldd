feab5d49c1675348643c8cd1f2b2e50de1205160
G0ONar <142931976+G0ONar@users.noreply.github.com>
2025-10-08 12:38:24 -0700

    fixed
    fixed

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,20 +1,20 @@
-//Gunnar.Pettit@malad.us
-//This is my hello world
+//Written by Gunnar Pettit
+//for Mr Gross's SD1 class assignment 1.3C
+//This is my Hello World
 public class HelloWorld
  {
-    public static void main(string[] args)[]{
+    public static void main(String[] args)[]{
         int age =16;
         String name="Gunnar Pettit";
         System.out.println("Hellow world, I am "+name);
         System.out.println("I am "+age+"years old.");
         System.out.println("I have 3 siblings");
-        System.out.println("Ryker")
-        System.out.println("Maverick")
-        System.out.println("and Nora")
-        System.out.println("My parents are")
-        System.out.println("Beth")
-        System.out.println("and Kyle")
+        System.out.println("Ryker");
+        System.out.println("Maverick");
+        System.out.println("and Nora");
+        System.out.println("My parents are");
+        System.out.println("Beth");
+        System.out.println("and Kyle");
     }
 
-}
-
+ }
